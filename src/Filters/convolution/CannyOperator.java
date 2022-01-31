8c09c1a349472140982940baed2429fe88d34d84
Your Name <you@example.com>
2022-01-31 12:55:35 -0800

    convolution

---
+++ src/Filters/convolution/CannyOperator.java
@@ -0,0 +1,17 @@
+package Filters.convolution;
+
+
+import core.DImage;
+
+public class CannyOperator {
+
+    public short[][] canny(short[][] canny) throws Exception {
+        DImage d = new DImage(canny[0].length, canny.length);
+        d.setPixels(canny);
+        Sobel sobel = new Sobel();
+        var v = sobel.processImage(d).getBWPixelGrid();
+        return v;
+    }
+
+
+}
